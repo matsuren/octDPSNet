590f71c199df2f024d016c65dc40cd02be9a4973
Ren Komatsu <komatsu@robot.t.u-tokyo.ac.jp>
2019-10-17 11:49:18 +0900

    Add install instruction in README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # OctDPSNet
-Pytorch implementation of [Octave Deep Plane-sweeping Network: Reducing Spatial Redundancy for Learning-based Plane-sweeping Stereo]. The more detailed information is coming soon.
+Pytorch implementation of [Octave Deep Plane-sweeping Network: Reducing Spatial Redundancy for Learning-based Plane-sweeping Stereo](./). 
+
+The more detailed information is coming soon.
 
 ## Demo
 You can try octDPSNet in Google Colab [here (octDPSNet_demo_colab.ipynb)](https://colab.research.google.com/github/matsuren/octDPSNet/blob/master/octDPSNet_demo_colab.ipynb).
@@ -9,11 +11,115 @@ After running all cells, point cloud `results.ply` will be downloaded. You can v
 Examples of the point cloud are displayed here.
 
 <span style="display:block;text-align:center">
-<img src="./sample_data/mvs_test_00023/out.gif" width="300px">
-<img src="./sample_data/sun3d_test_00047/out.gif" width="300px">
+<img src="./sample_data/mvs_test_00023/out.gif" width="350px">
+<img src="./sample_data/sun3d_test_00047/out.gif" width="350px">
 </span>
 
 ## Requirements
+python >= 3.5  
+pipenv
+
+## Install
+We recommend you to use `pipenv` to install the correct version of the python libraries since some libraries (e.g. scipy) changed their API which causes some errors. We might modify our source code later to get it working on the latest version of the libraries.
+
+### Install pipenv
+```bash
+pip install pipenv
+```
+### Setup virtual environment
+```bash
+git clone https://github.com/matsuren/octDPSNet.git
+cd octDPSNet
+pipenv install
+```
+### Verify install
+Run the following command to make sure that you've installed octDPSNet correctly.
+```bash
+pipenv shell
+python demo.py
+```
+After running the command, you will see point cloud in Open3D visualization window and `results.ply` will be generated in the current directory.
+
+## Prepare dataset
+### Download DeMoN dataset
+We use [DeMoN datasets](https://lmb.informatik.uni-freiburg.de/people/ummenhof/depthmotionnet/) for training and testing.  
+Please follow the instruction [here (sunghoonim/DPSNet)](https://github.com/sunghoonim/DPSNet#data-praparation) to prepare the datasets.
+
+For the preparation of ETH3D dataset, please see [here](#Download-dataset).
+
+
+## Train
+
+
+### Training
+
+```bash
+python train.py
+$DATASETROOT/home/komatsu/datasets/demon/train/ --log-output --alpha 0.75
+```
+
+**Note:** If you want to try other alpha value (`--alpha`), please check [How to choose α](#How-to-choose-α).
+
+### Reduce GPU memory consumption
+If you don't have enough GPU memory.
+Set smaller number for `--nlabel` (Default is 64), `--batch-size` (Default is 16).
+<!-- Default value for `--alpha` is 0.9375, so you don't have to change it. -->
+E.g.,
+```bash
+python train.py
+$DATASETROOT/home/komatsu/datasets/demon/train/ --log-output --nlabel 16 --batch-size 8
+```
+## Test
+### DeMoN dataset
+
+```bash
+cd $octDPSNet
+python test.py $DATASETROOT/hogehoge --alpha 0.75
+```
+**Note:** If you want to try your own pretrained model, please add a flag `--pretrained $PATH_TO_MODEL`.
+
+### ETH3D dataset
+
+#### Download dataset
+Download ETH3D dataset from [DeepMVS project page](https://phuang17.github.io/DeepMVS/index.html) and [sunghoonim/DPSNet](https://github.com/sunghoonim/DPSNet) by executing the following commands.
+
+```bash
+cd $DATASETROOT
+wget https://www.dropbox.com/s/n26v56eqj0jpd60/ETH3D_results.zip  
+unzip ETH3D_results.zip
+git clone --depth 1 https://github.com/sunghoonim/DPSNet.git DPSNetTmp
+cp -a DPSNetTmp/dataset/ETH3D_results .
+rm ETH3D_results.zip DPSNetTmp -rf
+```
+
+#### Test on ETH3D dataset
+
+In order to run command
+
+
+```bash
+cd $octDPSNet
+python test_ETH3D.py $DATASETROOT/ETH3D_results --sequence-length 2 --alpha 0.75
+```
+**Note:** If you want to try your own pretrained model, please add a flag `--pretrained $PATH_TO_MODEL`.
+
+## Test results on ETH3D dataset
+You can download the test results on ETH3D dataset [here (ETH3D_results.zip)](https://onedrive.live.com/?authkey=%21AG4yhtP0Yp39t6o&id=781BBA0C6BA3A88E%214672&cid=781BBA0C6BA3A88E).
+The directory structure is the same as [DeepMVS](https://phuang17.github.io/DeepMVS/index.html).
+
+
+## How to choose α
+OctDPSNet has a hyper-parameter α (`--alpha`) that controls the ratio of low spatial frequency features. For example, `α=0.75` implies that 75% of the features are low spatial frequency features. If α is increased, then less memory and computation resources are used, but useful information may be lost.
+
+We recommend you to choose α (`--alpha`) value from the following:
+
+- 0.25 &nbsp;&nbsp; ($C_h=24$ and $C_l=8$)
+- 0.5 &nbsp; &nbsp;&nbsp; ($C_h=16$ and $C_l=16$)
+- 0.75  &nbsp; &nbsp;($C_h=8$ and $C_l=24$)
+- 0.875  &nbsp;($C_h=4$ and $C_l=28$)
+- 0.9375 ($C_h=2$ and $C_l=30$)
+
+Here, $C_h$ and $C_l$ are the channel numbers of the high and low spatial frequency features, respectively
 
 ## Acknowledge
 This repository is based on [sunghoonim/DPSNet](https://github.com/sunghoonim/DPSNet). Thanks a lot.
---
+++ demo.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Demo for octDPSNet
+
+# In[ ]:
+
+
+# show pointclouds
+import open3d as o3d
+
+
+# In[ ]:
+
+
+from models.octDPSNet import octdpsnet
+import numpy as np
+
+import torch
+import torch.backends.cudnn as cudnn
+from models import octconv
+
+import cv2
+
+
+# In[ ]:
+
+
+############################
+# select data
+############################
+# test_folder = './sample_data/sun3d_test_00047/'
+test_folder = './sample_data/mvs_test_00023/'
+# test_folder = './sample_data/rgbd_test_00038/'
+
+
+# In[ ]:
+
+
+nlabel = 64
+mindepth = 0.5
+octconv.ALPHA = 0.75
+mymodel = octdpsnet(nlabel, mindepth, octconv.ALPHA, True).cuda()
+mymodel.eval()
+print('Load:')
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+def ToImg(img):
+    return img.cpu().numpy().transpose(1,2,0)/2 + 0.5
+
+def showImg(img):
+    ret = ToImg(img)
+    plt.imshow(ret)
+    
+def imread(fname):
+    img = cv2.imread(fname)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+def ToT(img):
+    img = torch.from_numpy(img.transpose(2, 0, 1)).float()/255
+    return (img-0.5)*2
+
+def preprocess(tgt_img, imgs, ref_poses, intrinsics):
+    tgt_img = ToT(tgt_img)[np.newaxis]
+    imgs = [ToT(it)[np.newaxis] for it in imgs]
+    ref_poses = [torch.from_numpy(it)[np.newaxis].float() for it in ref_poses]
+    inv_intrinsic = np.linalg.inv(intrinsics)
+    intrinsics = torch.from_numpy(intrinsics).float()
+    inv_intrinsic = torch.from_numpy(inv_intrinsic).float()
+    return tgt_img, imgs, ref_poses, intrinsics, inv_intrinsic
+
+
+# In[ ]:
+
+
+from functools import partial
+
+fname_l = test_folder + '/0000.jpg'
+fname_r = test_folder + '/0001.jpg'
+tgt_img = imread(fname_l)
+img = imread(fname_r)
+
+# intrinsics = np.array([[386.8946, 0, 382.7535],
+#                        [0, 386.8946, 219.6522],
+#                        [0, 0, 1]])
+# ref_poses = [np.array([[[0.9360, 0.0590, 0.3470, -566.4332],
+#                      [-0.0623, 0.9981, -0.0019, 17.1064],
+#                      [-0.3464, -0.0198, 0.9379,  123.5660]]])]
+
+intrinsics = np.loadtxt(test_folder + '/cam.txt')
+
+poses = np.loadtxt(test_folder + '/poses.txt')
+pose_tgt = np.concatenate((poses[0,:].reshape((3,4)), np.array([[0,0,0,1]])), axis=0)
+for j in [1]:
+    pose_src = np.concatenate((poses[j,:].reshape((3,4)), np.array([[0,0,0,1]])), axis=0)
+    pose_rel = pose_src.dot(np.linalg.inv(pose_tgt))
+    pose = pose_rel[:3,:].reshape((1,3,4)).astype(np.float32)
+ref_poses = [pose]
+
+# visualize
+fig, ax  = plt.subplots(1, 2, figsize=(12,9))
+ax = ax.flatten()
+
+ax[0].imshow(tgt_img)
+ax[1].imshow(img)
+
+
+# In[ ]:
+
+
+# preprocess image
+resize_x = 640
+resize_y = 480
+h, w, c = tgt_img.shape
+x_scale = resize_x/w
+y_scale = resize_y/h
+intrinsics[0] *= x_scale
+intrinsics[1] *= y_scale
+
+tgt_img = cv2.resize(tgt_img, (resize_x, resize_y))
+img = cv2.resize(img, (resize_x, resize_y))
+
+imgs = [img]  # imgs = [img1, img2]
+ref_poses = [pose]  # imgs = [pose1, pose2]
+
+org_img = np.array(tgt_img, dtype=np.uint8)
+
+tgt_img, imgs, ref_poses, intrinsics, inv_intrinsic = preprocess(tgt_img, imgs, ref_poses, intrinsics)
+
+showImg(tgt_img[0])
+
+
+# In[ ]:
+
+
+ref_poses
+
+
+# In[ ]:
+
+
+with torch.no_grad():
+    tgt_img = tgt_img.cuda()
+    imgs = [it.cuda() for it in imgs]
+    ref_poses = [it.cuda() for it in ref_poses]
+    intrinsics = intrinsics[np.newaxis].cuda()
+    inv_intrinsic = inv_intrinsic[np.newaxis].cuda()
+    ref_poses = torch.cat(ref_poses,1)
+    output_depth = mymodel(tgt_img, imgs, ref_poses, intrinsics, inv_intrinsic)    
+    depth = output_depth.squeeze().cpu().numpy()
+    
+print('depth max:', depth.max())
+print('depth min:', depth.min())
+plt.imshow(depth)
+
+
+# # Show PointCloud
+
+# In[ ]:
+
+
+def pcdCamera(scale=1.0, color=[1, 0, 0]):
+    points = [[-1, 0.75, 0],[1, 0.75, 0],[1, -0.75, 0],[-1, -0.75, 0],[0,0,0.6]]
+    lines = [[0,1],[1,2],[2,3],[3,0],
+             [0,4],[1,4],[2,4],[3,4]]
+    colors = [color for i in range(len(lines))]
+    line_set = o3d.geometry.LineSet()
+
+    points = np.array(points)*scale
+    line_set.points = o3d.utility.Vector3dVector(points)
+    line_set.lines = o3d.utility.Vector2iVector(lines)
+    line_set.colors = o3d.utility.Vector3dVector(colors)
+    return line_set
+
+
+# In[ ]:
+
+
+img = org_img
+resize_x = 640
+resize_y = 480
+img = cv2.resize(img, (resize_x, resize_y))
+
+height, width, _ = img.shape
+cam = intrinsics.cpu().numpy()[0]
+fx = cam[0,0]
+fy = cam[1,1]
+cx = cam[0,2]
+cy = cam[1,2]
+cam_o3 = o3d.camera.PinholeCameraIntrinsic(width, height, fx,fy,cx,cy)
+rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        o3d.geometry.Image(img), o3d.geometry.Image(depth), 
+    depth_scale=1.0, depth_trunc=100, convert_rgb_to_intensity=False)
+
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, cam_o3)
+print('save point cloud as results.ply')
+o3d.io.write_point_cloud('results.ply', pcd)
+
+colors = [[i==0, i==1, i==2] for i in range(3)]
+d = depth
+camera_scale = d[d>0.01].min()/9
+cam_ext = ref_poses.cpu().numpy()[0]
+cam_pcds = []
+cam_pcd = pcdCamera(-camera_scale, colors[0])
+cam_pcds.append(cam_pcd)
+
+for i, it in enumerate(cam_ext):
+    cam_pcd = pcdCamera(-camera_scale, colors[i+1])
+    T = np.concatenate((it, np.array([[0,0,0,1]])), axis=0)
+    invT = np.linalg.inv(T)
+    cam_pcd.transform(invT)
+    cam_pcds.append(cam_pcd)
+
+show_pcds = [pcd, *cam_pcds]
+for it in show_pcds:
+    it.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+o3d.visualization.draw_geometries(show_pcds)
